4fb8f03823c688d0ff33a0fb79530f0677326b69
zzw <dgzzw1@163.com>
2022-12-26 21:42:38 +0800

    我的第一个Java程序

---
+++ java-foundational/src/main/java/org/example/java/foundational/object/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.example.java.foundational.object;
+
+/**
+ * 我的第一个Java程序
+ *
+ * @author zzw dgzzw1@163.com
+ * @version 2022/12/26 21:37
+ * @since JDK17
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world with mac11.0 & JDK17 & Git" +
+				"& Maven &IntelliJ IDEA 2022");
+	}
+
+}
